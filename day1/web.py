3ad9e21db701778fbfd79958cc0826b483554f41
chenguangtao@seevision.cn <792607724@qq.com>
2025-02-10 11:29:38 +0800

    push to github

---
+++ day1/web.py
@@ -0,0 +1,33 @@
+# coding = utf8
+import sys
+
+sys.path.append("../../")
+import os
+from flask import Flask, render_template
+
+os.path.abspath(".")
+
+"""
+    @Project:SeevisionTestDepartmentWebProject
+    @File:web.py
+    @Author:十二点前要睡觉
+    @Date:2025/2/9 20:21
+"""
+
+app = Flask(__name__)
+
+
+# print(__name__)
+
+# 创建了网址 /show/info 和 函数index 的对应关系
+# 以后用户在浏览器访问 /show/info，网站自动执行 index
+@app.route("/show/info")
+def index():
+    # return "中<h1>国</h1><span style='color:red;'>联通</span>"
+    # Flask 内部会自动打开这个文件，并读取内容，将内容给用户返回
+    # 默认：去当前项目目录的templates文件夹去找
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run()
--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+"""
+    Recording:
+    目的：开发一个平台（网站）
+    前端开发：HTML、CSS、JavaScript
+    Web框架：接收请求并处理
+    MySQL数据库：存储数据的地方
 
-# Press Alt+Shift+1 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    快速上手：基于Flask框架让你快速搭建一个网站出来。
+
+    深入学习：基于Django框架（主要）
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    1、快速开发一个网站:
+    pip3 install flask
+
+    2、浏览器能识别的标签：
+    2.1 编码：（head）
+    <meta charset="UTF-8">
+
+    2.2 title：（head）
+    <title>我的联通</title>
+
+    2.3 标题：
+    <h1>1级标题</h1>
+    ……
+
+    2.4 div和span：（这两个标签很素）可以扩展+CSS提升样式效果
+    div：一个人占一整行。【块级标签】
+    span：自己有多大就占多少。【行内标签、内联标签】
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+"""
