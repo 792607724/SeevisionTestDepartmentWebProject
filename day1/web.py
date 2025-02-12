e360fe57d688a1afbef65563335252044805e383
chenguangtao@seevision.cn <792607724@qq.com>
2025-02-12 22:15:48 +0800

    push to github

--- day1/web.py
+++ day1/web.py
@@ -29,5 +29,10 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/get/news")
+def get_news():
+    return render_template("get_news.html")
+
+
 if __name__ == '__main__':
     app.run()
